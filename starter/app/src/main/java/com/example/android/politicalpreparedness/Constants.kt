b79000304de37559793b8ddab66f82ca2e2bee95
Marwa Abou Niaaj <marwa.niaaj@gmail.com>
2021-04-24 14:48:52 +0400

    Remove API key

--- starter/app/src/main/java/com/example/android/politicalpreparedness/Constants.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/Constants.kt
@@ -2,7 +2,7 @@ package com.example.android.politicalpreparedness
 
 object Constants {
     const val BASE_URL = "https://www.googleapis.com/civicinfo/v2/"
-    const val API_KEY = "AIzaSyA6JfufomUR1yB0Hy3xUqG7s0DTKJQ8C0M"
+    const val API_KEY = "API_KEY"
     const val ELECTION_QUERY = "elections"
     const val VOTER_INFO_QUERY = "voterinfo"
     const val REPRESENTATIVE_QUERY = "representatives"
